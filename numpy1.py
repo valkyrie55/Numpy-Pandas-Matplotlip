94384fb578847bc41c1167bc0bec7f590a7d8b5c
Shweta <38096594+valkyrie55@users.noreply.github.com>
2018-09-20 16:51:49 +0530

    Add files via upload

---
+++ numpy1.py
@@ -0,0 +1,9 @@
+import numpy as np
+#create a list
+l = [1,2,3,4,5]
+print(l)
+#create an ndarray through the list
+arr1 =np.array(l)
+print(arr1)
+print("Datatype of the ndarray:")
+print(arr1.dtype)
