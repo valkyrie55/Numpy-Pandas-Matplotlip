55f5440c439f1b5bebd457ea7375854524b691ae
Shweta <38096594+valkyrie55@users.noreply.github.com>
2018-09-20 17:41:49 +0530

    Update numpy_basics_begin.py

--- numpy_basics_begin.py
+++ numpy_basics_begin.py
@@ -34,3 +34,9 @@ print("-----------------------")
 print(data4_)
 print(data4_*data4_)
 print(data4_ + data4_)
+a = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(a)
+#--------------------------
+#----slicing in ndarray-----
+print(a[1])
+print(a[1,1])
